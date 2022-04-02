d1b28c1972180577ed2653f80e0b6a8215e1ad97
IsabelaCortesB <isabelacortesbuiles@hotmail.com>
2022-04-01 23:18:58 -0500

    más avances

--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
 var pantalla1
 var pantalla2
 var pantalla3
+var contenedorBotones
+var audiosrc
+var audio
+
+var playlists = {
+  playlistA: ["Nieve.mp3", "LaPuertaEsElAmor.mp3"],
+  playlistElsa: ["LetItGo.mp3", "MuchoMásAllá.mp3", "Sueltalo.mp3"],
+  playlistOlaf: ["Muéstrate.mp3", "ShowYourself.mp3"]
+
+
+}
 
 function setup() {
    pantalla1 = select("#Pantalla1");
    pantalla2 = select("#Pantalla2");
    pantalla3 = select("#Pantalla3");
-   
-  var botonInicio = select("#botonInicio");
+   contenedorBotones = select("#contenedorBotones");
+   audiosrc = select("#audiosrc");
+   audio = select("#audio");
+
   
+  var botonInicio = select("#botonInicio");
+  var fileinput = createFileInput(handleFile);
   botonInicio.mousePressed(botonInicioPressed)
 
   var botonRegresar = select("#botonRegresar");
@@ -29,11 +44,48 @@ function botonesRecuadrosPressed(){
 
 pantalla2.hide();
 pantalla3.show();
-console.log(this);
+
+
 var recuadroPlaylist = select("#recuadroPlaylist")
 recuadroPlaylist.attribute("src",this.attribute("src"))
 
 
+
+var botones = selectAll("#audioButton")
+for (var i=0; i < botones.length; i++){
+  var boton = botones[i] 
+  boton.remove();
+ 
+}
+
+var playlistName = this.attribute("id")
+var audios = playlists[playlistName]
+
+for (var i=0; i < audios.length; i++){
+  var audio = audios[i] 
+  crearAudioButton(audio, playlistName);
+ 
+}
+
+}
+
+
+function crearAudioButton(audioName, playlistName) {
+  var audioButton = createButton(audioName)
+  audioButton.attribute("id","audioButton")
+  audioButton.mousePressed(()=>{
+  audio.stop();
+    audiosrc.attribute("src",playlistName+"/" +audioName)
+    audio.play();
+  })
+  contenedorBotones.child(audioButton);
+
+
+}
+
+function handleFile(file){
+
+console.log(file);
 }
 
 
